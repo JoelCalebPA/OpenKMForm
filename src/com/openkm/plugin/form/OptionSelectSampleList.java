7cbd1cda11ff5f4ba19677464b3d0dd3b0254eb6
Josep Llort <jllort@openkm.com>
2015-02-14 17:11:35 +0000

    initial import
    git-svn-id: http://forge.openkm.com/public/form-sample@6 629e130e-4a86-4a19-b771-f440e8d9c08d

---
+++ src/com/openkm/plugin/form/OptionSelectSampleList.java
@@ -0,0 +1,57 @@
+/**
+ * OpenKM, Open Document Management System (http://www.openkm.com)
+ * Copyright (c) 2006-2015 Paco Avila & Josep Llort
+ * 
+ * No bytes were intentionally harmed during the development of this application.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.openkm.plugin.form;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.openkm.api.OKMAuth;
+import com.openkm.bean.form.Option;
+import com.openkm.principal.PrincipalAdapterException;
+
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+
+@PluginImplementation
+public class OptionSelectSampleList implements OptionSelectValues {
+	@Override
+	public String getName() {
+		return "User list";
+	}
+	
+	@Override
+	public List<Option> getOptions() throws OptionSelectException {
+		List<Option> options = new ArrayList<Option>();
+		try {
+			// Load select values. In the sample use users list from api call, but also could be thirdparty call or sql to openkm database ( see LegacyDAO)
+			List<String> userList; userList = OKMAuth.getInstance().getUsers(null);
+			for (String user : userList) {
+				Option option = new Option();
+				option.setLabel(OKMAuth.getInstance().getName(null, user));
+				option.setValue(user);
+				options.add(option);
+			}
+		} catch (PrincipalAdapterException e) {
+			throw new OptionSelectException(e);
+		}
+		return options;
+	}
+}
